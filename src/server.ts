1f64c6dcb68c349b62a0a945f585bd1456699a1d
Thomas Couto <thomascouto@gmail.com>
2022-02-25 15:27:53 -0300

    chore: update ports

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import { setupRoutes } from './main/config/setupRoutes'
 import { err } from './app/util/httpHelper'
 // import cors from 'cors'
 
-const port = process.env.SERVER_PORT || 3000
+const port = process.env.SERVER_PORT || 8080
 
 ;(async () => {
   const app = express()
