b77bb46b15cd276f8e601d88d0f4c89b967d4ba8
Thomas Couto <thomascouto@gmail.com>
2022-02-25 15:19:46 -0300

    chore: update main

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import './main/config/moduleAlias'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { RequestContext } from '@mikro-orm/core'
 import { initializeORM } from './main/config/mikro-orm.pg'
 import { setupRoutes } from './main/config/setupRoutes'
 import { err } from './app/util/httpHelper'
-import cors from 'cors'
+// import cors from 'cors'
 
 const port = process.env.SERVER_PORT || 3000
 
@@ -12,12 +12,14 @@ const port = process.env.SERVER_PORT || 3000
   const app = express()
   const DI = await initializeORM()
 
-  app.use(cors())
+  // app.use(cors())
   app.use(express.json())
-  app.use((req, res, next) => RequestContext.create(DI.em, next))
+  app.use((req: Request, res: Response, next: NextFunction) =>
+    RequestContext.create(DI.em, next)
+  )
   app.use(express.urlencoded({ extended: false }))
   setupRoutes(app)
-  app.use((req, res) =>
+  app.use((req: Request, res: Response) =>
     res.status(404).json(err(404, new Error('Route not found.')))
   )
 
