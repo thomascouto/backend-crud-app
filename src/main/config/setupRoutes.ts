75cde577bb908039dc9d5aa29770f9236c95ab3a
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2022-03-14 16:39:10 -0300

    fix: bug correction on recurssive function

--- src/main/config/setupRoutes.ts
+++ src/main/config/setupRoutes.ts
@@ -2,20 +2,19 @@ import { Router, Express } from 'express'
 import { readdirSync } from 'fs'
 import { join } from 'path'
 
-let CURRENT_DIR = join(__dirname, '../routes')
+const BASE_DIR = join(__dirname, '../routes')
 export const setupRoutes = (app: Express): void => {
   const router = Router()
-  recursiveReading(CURRENT_DIR, router)
+  recursiveReading(BASE_DIR, router)
   app.use('/', router)
 }
 
 const recursiveReading = (dir: string, router: Router): void => {
   readdirSync(dir, { withFileTypes: true }).forEach(async (element) => {
     if (element.isFile()) {
-      ;(await import(`${CURRENT_DIR}/${element.name}`)).default(router)
-    } else {
-      CURRENT_DIR = join(CURRENT_DIR, element.name)
-      recursiveReading(CURRENT_DIR, router)
+      ;(await import(`${dir}/${element.name}`)).default(router)
+    } else if (element.isDirectory()) {
+      recursiveReading(`${BASE_DIR}/${element.name}`, router)
     }
   })
 }
